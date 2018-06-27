1b2284ce6aa6f5a7e0412e5f9456b48d9ea3f195
Саша Калаш <kalash@Sasha-Kalash.local>
2018-06-27 21:14:09 +0300

    rest test: post request to order

--- test/rest-test.js
+++ test/rest-test.js
@@ -6,22 +6,46 @@ const app = require('../index');
 
 
 describe('REST API', () => {
+  let server;
   before(() => {
+    server = supertest(app);
   });
   it('GET /order returned status 200 & array', done => {
-    supertest(app)
+    server
       .get('/order')
       .set('Accept', 'application/json')
       .set('Content-Type', 'application/json')
       .end((err, res) => {
-        if (err) { 
+        if (err) {
           console.error(err);
-          done(); 
+          done();
         }
         assert.equal(res.status, 200);
         assert.ok(res.body);
         expect(res.body).to.be.a('array');
-        done(); 
+        done();
       });
+  });
+  it('POST /order returned status 200 & object with status "Заказано"', done => {
+    server
+      .post('/order')
+      .send({
+        title: 'cake',
+        status: 'Заказано'
+      })
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .end((err, res) => {
+        if (err) {
+          console.error(err);
+          done();
+        }
+        assert.equal(res.status, 200);
+        assert.ok(res.body);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('status').to.be('Заказано');
+        assert.equal(res.body.status, 'Заказано');
+        done();
+    });
   });
 });
